33ad9d575bf7e594b7d27a033eab03f65c9ce97a
Nick Lavery <tuneboynick@gmail.com>
2020-08-20 22:25:45 -0400

    Implementations of Logos and Products

--- src/Categories.js
+++ src/Categories.js
@@ -1,22 +1,22 @@
 import React from 'react'
 
-
-class Logos extends React.Component {
+class Categories extends React.Component {
     constructor() {
         super()
         this.state = {
             response: []
         }
-    };
+    }
+
 
     componentDidMount() {
         this.callApi()
-            .then(response => this.setState({ response: response.length + ' logos found.' }))
+            .then(response => this.setState({ response: response.length + ' number of categories.' }))
             .catch(error => console.log(error));
     };
 
     callApi = async () => {
-        const response = await fetch('http://localhost:3001/logos');
+        const response = await fetch('http://localhost:3000/categories')
         const body = await response.json();
         if (response.status !== 200) throw Error(body.message);
 
@@ -26,14 +26,11 @@ class Logos extends React.Component {
     render() {
         return (
             <div>
-                <div>Logos Place Holder</div>
+                <div>Categories Place Holder</div>
                 <div>{this.state.response}</div>
             </div>
         )
     }
-
 }
 
-
-
-export default Logos
+export default Categories
